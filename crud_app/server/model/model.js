4a344d65433b5e7b673b9e38870d30e20be90da8
ivan-ursta <ursta2017@gmail.com>
2022-09-13 17:46:49 +0300

    finish commit

---
+++ crud_app/server/model/model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+    firstname:{
+        type:String,
+        required:true
+    },
+    lastname:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    phone:{
+        type:String,
+        required:true,
+        unique:true
+    }
+})
+const Userdb = mongoose.model('userdb', schema);
+
+module.exports = Userdb;
